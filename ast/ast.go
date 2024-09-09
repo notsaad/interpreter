e7cf6595b4ffa6aa7c82216c534a1811d470c98f
notsaad <saadmazharr@gmail.com>
2024-09-09 13:18:37 -0400

    implemented basic testing and frameworks for pratt parser implementation

--- ast/ast.go
+++ ast/ast.go
@@ -8,6 +8,17 @@ import (
 
 type Node interface {
     TokenLiteral() string
+    String() string // main purpose of this is to be able to print nodes for debugging and comparing node values
+}
+
+func (p *Program) String() string {
+
+    var out bytes.Buffer // creates a buffer
+
+    for _, s:= range p.Statements {
+        out.WriteString(s.String()) // writes the return value of each statements String() method to it
+    }
+    return out.String() // returns the buffer as a string
 }
 
 type Statement interface {
@@ -33,15 +44,50 @@ func (p *Program) TokenLiteral() string {
     }
 }
 
+type ExpressionStatement struct {
+    Token       token.Token // the first token of the expression
+    Expression  expression
+}
+
 type LetStatement struct {
     Token   token.Token // the token.LET token
     Name    *Identifier
     Value   Expression
 }
 
+func (ls *LetStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Name.String())
+    out.WriteString(" = ")
+
+    if ls.Value != nil {
+        out.WriteString(ls.Value.String())
+    }
+
+    out.WriteString(";")
+
+    return out.String()
+}
+
 type ReturnStatement struct {
     Token       token.Token // the 'return' toen
     ReturnValue Expression // contains the expression that is to be returned
+}
+
+func (rs *ReturnStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(rs.TokenLiteral() + " ")
+
+    if rs.ReturnValue != nil {
+        out.WriteString(rs.ReturnValue.String())
+    }
+
+    out.WriteString(";")
+
+    return out.String()
 }
 
 func (rs *ReturnStatement) statementNode() {}
@@ -56,6 +102,19 @@ func (ls *LetStatement) TokenLiteral() string {
     return ls.Token.Literal
 }
 
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+    return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+    if es.Expression != nil {
+        return es.Expression.String()
+    }
+    return ""
+}
+
 // holds the identifier of the binding (x in 'let x = 5;')
 type Identifier struct {
     Token   token.Token // the token.IDENT token
@@ -66,3 +125,4 @@ type Identifier struct {
 // expression nodes could also be identifiers in situations like 'let x = 5'
 func (i *Identifier) expressionNode()           {}
 func (i *Identifier) TokenLiteral() string      { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
