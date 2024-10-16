a85cabe6a2ecff284034102729a313e082b15ee8
notsaad <saadmazharr@gmail.com>
2024-10-15 17:21:43 -0700

    functional integer literal evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,9 +7,24 @@ import (
 
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
-        case *ast.IntegerLiteral:
-            return &object.Integer{Value: node.Value}
-    }
+        
+        // statements
+    case *ast.Program:
+        return evalStatements(node.Statements)
+    case *ast.ExpressionStatement:
+        return Eval(node.Expression) // recursive evaluation call
+        // expressions
+    case *ast.IntegerLiteral:
+        return &object.Integer{Value: node.Value}
 
+    }
     return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+    var result object.Object
+    for _, statement := range stmts {
+        result = Eval(statement)
+    }
+    return result
 }
