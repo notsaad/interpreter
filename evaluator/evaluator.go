50abe9653dbe9312d3ef0683c4b0b57b7d5448c1
notsaad <saadmazharr@gmail.com>
2024-10-15 17:39:33 -0700

    minimized creating of new objects for boolean literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
     "skibidi/object"
 )
 
+var (
+    NULL  = &object.Null{}
+    TRUE  = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
         
@@ -18,7 +24,7 @@ func Eval(node ast.Node) object.Object {
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
     case *ast.Boolean:
-        return &object.Boolean{Value: node.Value}
+        return boolToBooleanObj(node.Value)
 
     }
     return nil
@@ -30,4 +36,12 @@ func evalStatements(stmts []ast.Statement) object.Object {
         result = Eval(statement)
     }
     return result
+}
+
+func boolToBooleanObj(input bool) *object.Boolean {
+    if input {
+        return TRUE
+    } else {
+        return FALSE
+    }
 }
