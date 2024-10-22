44c1d04150c4c11f2aec87ecbea7a321e2dfab90
notsaad <saadmazharr@gmail.com>
2024-10-22 18:42:27 -0400

    successful evaluating for conditionals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -21,6 +21,8 @@ func Eval(node ast.Node) object.Object {
         return evalStatements(node.Statements)
     case *ast.ExpressionStatement:
         return Eval(node.Expression) // recursive evaluation call
+    case *ast.BlockStatement:
+        return evalStatements(node.Statements)
 
     // expressions
     case *ast.IntegerLiteral:
@@ -34,6 +36,8 @@ func Eval(node ast.Node) object.Object {
         left := Eval(node.Left)
         right := Eval(node.Right)
         return evalInfixExpression(node.Operator, left, right)
+    case *ast.IfExpression:
+        return evalIfExpression(node)
 
     }
     return nil
@@ -127,4 +131,28 @@ func evalIntegerInfixExpression(operator string, left object.Object, right objec
         return NULL
     }
 
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+    condition := Eval(ie.Condition)
+    if isTruthy(condition) {
+        return Eval(ie.Consequence)
+    } else if (ie.Alternative != nil) {
+        return Eval(ie.Alternative)
+    } else{
+        return NULL
+    }
+}
+
+func isTruthy(obj object.Object) bool {
+    switch obj {
+    case NULL:
+        return false
+    case TRUE:
+        return true
+    case FALSE:
+        return false
+    default:
+        return true
+    }
 }
