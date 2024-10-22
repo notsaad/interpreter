f4f4dfbcf1739e0d227b5118ba29fc943cd19638
notsaad <saadmazharr@gmail.com>
2024-10-22 19:27:06 -0400

    added tests and object type for error handling

--- object/object.go
+++ object/object.go
@@ -11,6 +11,7 @@ const (
     BOOLEAN_OBJ = "BOOLEAN"
     NULL_OBJ    = "NULL"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
+    ERROR_OBJ = "ERROR"
 )
 
 // every value in the source code will be represented as an object for simplicity
@@ -65,4 +66,16 @@ func (rv *ReturnValue) Type() ObjectType {
 
 func (rv *ReturnValue) Inspect() string {
     return rv.Value.Inspect()
+}
+
+type Error struct {
+    Message string
+}
+
+func (e *Error) Type() ObjectType {
+    return ERROR_OBJ
+}
+
+func (e *Error) Inspect() string {
+    return "ERROR: " + e.Message
 }
