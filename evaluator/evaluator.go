f409f4a8649a3ba47bc154613d9622a67482866b
notsaad <saadmazharr@gmail.com>
2024-10-22 19:16:05 -0400

    evaluation for return statements

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -18,11 +18,11 @@ func Eval(node ast.Node) object.Object {
         
         // statements
     case *ast.Program:
-        return evalStatements(node.Statements)
+        return evalProgram(node)
     case *ast.ExpressionStatement:
         return Eval(node.Expression) // recursive evaluation call
     case *ast.BlockStatement:
-        return evalStatements(node.Statements)
+        return evalBlockStatement(node)
 
     // expressions
     case *ast.IntegerLiteral:
@@ -38,6 +38,9 @@ func Eval(node ast.Node) object.Object {
         return evalInfixExpression(node.Operator, left, right)
     case *ast.IfExpression:
         return evalIfExpression(node)
+    case *ast.ReturnStatement:
+        val := Eval(node.ReturnValue)
+        return &object.ReturnValue{Value: val}
 
     }
     return nil
@@ -47,6 +50,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
     var result object.Object
     for _, statement := range stmts {
         result = Eval(statement)
+
+        if returnValue, ok := result.(*object.ReturnValue); ok {
+            return returnValue.Value
+        }
+
     }
     return result
 }
@@ -156,3 +164,31 @@ func isTruthy(obj object.Object) bool {
         return true
     }
 }
+
+// a more specialized fn to evaluate block statements
+func evalProgram(program *ast.Program) object.Object {
+    var result object.Object
+    
+    for _, stmt := range program.Statements {
+        result = Eval(stmt)
+
+        if returnValue, ok := result.(*object.ReturnValue); ok {
+            return returnValue.Value
+        }
+    }
+    return result
+}
+
+// so the inner most blocked return statement is returned, and the rest of the block is not evaluated
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+    var result object.Object
+
+    for _, stmt := range block.Statements {
+        result = Eval(stmt)
+        if result != nil && result.Type() == object.RETURN_VALUE_OBJ{
+            return result
+        }
+    }
+    return result
+}
+
